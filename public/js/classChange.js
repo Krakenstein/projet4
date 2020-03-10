e472c243b51294643d728e7cf15a141d76fdc6d9
SebH2as <se.haas@laposte.net>
2020-03-10 10:24:22 +0100

    js

--- public/js/classChange.js
+++
@@ -1,9 +0,0 @@
-var reporting = document.getElementsByClassName("reporting");
-
-for (var i = 0; i < reporting.length; i++) {
-	reporting[i].addEventListener("click", function() {
-				  
-        this.innerText = "Commentaire signalé";
-        this.className = "reported";
-	});
-}
